42282a711ed9409a6bd72cc7a0181ea23623fb11
Michael Albertsen <culex@culex.dk>
2021-04-13 13:53:25 +0200

    Update xoops_version.php

--- xoops_version.php
+++ xoops_version.php
@@ -26,7 +26,7 @@ $moduleDirName = basename(__DIR__);
 
 // ------------------- Informations ------------------- //
 $modversion = [
-    'version'             => 1.26,
+    'version'             => 1.28,
     'module_status'       => 'Final',
     'release_date'        => '2021/01/06',
     'name'                => _MI_SMALLWORLD_MODULE_NAME,
@@ -145,7 +145,6 @@ $modversion['blocks'][] = [
     'show_func'   => 'smallworld_blocklist',
     'template'    => 'smallworld_block.tpl',
 ];
-
 //Module Configs
 
 $modversion['config'][] = [
